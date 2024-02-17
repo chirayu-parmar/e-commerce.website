c76a55b30c61b41a2b20f26608b4d485fc8ac746
parmar-chirayu <cyparmar@my.yorku.ca>
2024-02-16 23:51:11 -0500

    Update 4
    Basic Product FrontEnd

--- src/main/java/com/ecommerce/website/dao/ProductCategoryRepository.java
+++ src/main/java/com/ecommerce/website/dao/ProductCategoryRepository.java
@@ -3,7 +3,8 @@ package com.ecommerce.website.dao;
 import com.ecommerce.website.Entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
+@CrossOrigin("http://localhost:4200/")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
--- src/main/java/com/ecommerce/website/dao/ProductRepository.java
+++ src/main/java/com/ecommerce/website/dao/ProductRepository.java
@@ -2,7 +2,9 @@ package com.ecommerce.website.dao;
 
 import com.ecommerce.website.Entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200/")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
